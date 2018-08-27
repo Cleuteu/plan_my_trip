a25a479fc0cd3a3f6d1d1828a0334f6cdcab9036
Thomas Pouillevet <thomas.pouillevet@gmail.com>
2018-08-27 18:46:17 +0200

    preparation des seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,48 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Review.destroy_all
+Message.destroy_all
+Renting.destroy_all
+Suit.destroy_all
+User.destroy_all
+
+puts 'Creating users, suits, rentings and messages...'
+
+user = User.new(
+    first_name: 'Toto',
+    last_name: 'Dupont',
+    email: 'toto@gmail.com',
+    encrypted_password: 'totototo',
+    # reset_password_token: '',
+    # reset_password_sent_at: '',
+    # remember_created_at: '',
+    # created_at: ,
+    # updated_at", null: false
+    # t.string "photo" ,
+  )
+
+
+trip = Trip.new(
+    name: 'Voyage aux USA',
+    start_location: 'New-York',
+    end_location: 'Dallas',
+    # reset_password_token: '',
+    # reset_password_sent_at: '',
+    # remember_created_at: '',
+    # created_at: ,
+    # updated_at", null: false
+    # t.string "photo" ,
+  )
+
+user.save!
+trip.save!
+
+user_trip = UserTrip.new
+
+user_trip.user = user
+user_trip.trip = trip
+
+user_trip.save!
