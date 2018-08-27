9aaea0ea357fb2208049b24c6dfb87f24b709cca
Alex Marichal <marichalalex@yahoo.fr>
2018-08-27 18:44:41 +0200

    added fields to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,9 @@ class User < ApplicationRecord
 
   has_many :user_trips
   has_many :trips
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+
+  mount_uploader :photo, PhotoUploader
 end
