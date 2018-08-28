04e85cbea4d87e7ffdedf23b0c0d562fb7b271b0
Alex Marichal <marichalalex@yahoo.fr>
2018-08-29 00:36:18 +0200

    text on!

--- app/javascript/plugins/init_mermaid.js
+++ app/javascript/plugins/init_mermaid.js
@@ -4,4 +4,19 @@ function init_mermaid() {
   mermaid.initialize({startOnLoad:true});
 }
 
+
+function textCircle() {
+  setTimeout(() => {
+    const circles = document.querySelectorAll(".node")
+    console.log(circles)
+    circles.forEach((circle) => {
+        var id = console.log(circle.getAttribute('id'));
+        var text ="<text x=20 y=40 transform=translate(10,35)>#{event.name}</text>"
+        circle.insertAdjacentHTML("afterbegin", text)
+    })
+  }, 2000)
+};
+
+
 export { init_mermaid }
+export { textCircle }
