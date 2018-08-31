a54eb1f10ec05f48e96c62ed2d98f939e16f1ad9
Thomas Pouillevet <thomas.pouillevet@gmail.com>
2018-08-31 10:09:05 +0200

    js geo

---
+++ app/javascript/components/autocomplete.js
@@ -0,0 +1,16 @@
+function autocomplete() {
+  document.addEventListener("DOMContentLoaded", function() {
+    var flatAddress = document.getElementById('flat_address');
+
+    if (flatAddress) {
+      var autocomplete = new google.maps.places.Autocomplete(flatAddress, { types: [ 'geocode' ] });
+      google.maps.event.addDomListener(flatAddress, 'keydown', function(e) {
+        if (e.key === "Enter") {
+          e.preventDefault(); // Do not submit the form on Enter.
+        }
+      });
+    }
+  });
+}
+
+export { autocomplete };
