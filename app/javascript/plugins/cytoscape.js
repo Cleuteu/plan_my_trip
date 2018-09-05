e059c2ae15f0ae488084621026c15030a85efe6d
Eugene Ernoult <e.ernoult@gmail.com>
2018-09-05 12:59:05 +0200

    installed cy edgehandles

--- app/javascript/plugins/cytoscape.js
+++ app/javascript/plugins/cytoscape.js
@@ -2,8 +2,10 @@ import cytoscape from 'cytoscape';
 import tippy from 'tippy.js'
 import popper from 'cytoscape-popper';
 import '@fortawesome/fontawesome-free/css/all.min.css';
+import edgehandles from 'cytoscape-edgehandles';
 
 cytoscape.use( popper );
+cytoscape.use( edgehandles );
 
 
 const graph = document.getElementById('cy');
@@ -344,6 +346,14 @@ const cy = cytoscape({
         'width': 10,
       }
     },{
+      selector: ".eh-handle",
+      style: {
+        // 'color': 'black',
+        'width': 20,
+        'height': 20,
+        'background-color': '#54B589',
+      }
+    },{
       selector: 'core',
       style: {
         'active-bg-opacity': 0
@@ -562,4 +572,88 @@ ref_array.forEach((ref_node) => {
 //     // event.currentTarget.classList.toggle("img-circle");
 //   });
 // });
+
+// TEST HANDLES POUR ADD BRANCH
+
+let add_eventnodes = cy.elements("node")
+const array = add_eventnodes.filter(node => {
+  return node.connectedEdges().length === 1;
+});
+
+console.log(add_eventnodes)
+
+let eh_defaults = {
+  preview: true, // whether to show added edges preview before releasing selection
+  hoverDelay: 150, // time spent hovering over a target node before it is considered selected
+  handleNodes: 'node', // selector/filter function for whether edges can be made from a given node
+  snap: false, // when enabled, the edge can be drawn by just moving close to a target node (can be confusing on compound graphs)
+  snapThreshold: 50, // the target node must be less than or equal to this many pixels away from the cursor/finger
+  snapFrequency: 15, // the number of times per second (Hz) that snap checks done (lower is less expensive)
+  noEdgeEventsInDraw: false, // set events:no to edges during draws, prevents mouseouts on compounds
+  disableBrowserGestures: true, // during an edge drawing gesture, disable browser gestures such as two-finger trackpad swipe and pinch-to-zoom
+  handlePosition: function( node ){
+    return 'middle bottom'; // sets the position of the handle in the format of "X-AXIS Y-AXIS" such as "left top", "middle top"
+  },
+  handleInDrawMode: false, // whether to show the handle in draw mode
+  edgeType: function( sourceNode, targetNode ){
+    // can return 'flat' for flat edges between nodes or 'node' for intermediate node between them
+    // returning null/undefined means an edge can't be added between the two nodes
+    return 'flat';
+  },
+  loopAllowed: function( node ){
+    // for the specified node, return whether edges from itself to itself are allowed
+    return false;
+  },
+  nodeLoopOffset: -50, // offset for edgeType: 'node' loops
+  nodeParams: function( sourceNode, targetNode ){
+    // for edges between the specified source and target
+    // return element object to be passed to cy.add() for intermediary node
+    return {};
+  },
+  edgeParams: function( sourceNode, targetNode, i ){
+    // for edges between the specified source and target
+    // return element object to be passed to cy.add() for edge
+    // NB: i indicates edge index in case of edgeType: 'node'
+    return {};
+  },
+  show: function( sourceNode ){
+    // fired when handle is shown
+  },
+  hide: function( sourceNode ){
+    // fired when the handle is hidden
+  },
+  start: function( sourceNode ){
+    // fired when edgehandles interaction starts (drag on handle)
+  },
+  complete: function( sourceNode, targetNode, addedEles ){
+    // fired when edgehandles is done and elements are added
+  },
+  stop: function( sourceNode ){
+    // fired when edgehandles interaction is stopped (either complete with added edges or incomplete)
+  },
+  cancel: function( sourceNode, cancelledTargets ){
+    // fired when edgehandles are cancelled (incomplete gesture)
+  },
+  hoverover: function( sourceNode, targetNode ){
+    // fired when a target is hovered
+  },
+  hoverout: function( sourceNode, targetNode ){
+    // fired when a target isn't hovered anymore
+  },
+  previewon: function( sourceNode, targetNode, previewEles ){
+    // fired when preview is shown
+  },
+  previewoff: function( sourceNode, targetNode, previewEles ){
+    // fired when preview is hidden
+  },
+  drawon: function(){
+    // fired when draw mode enabled
+  },
+  drawoff: function(){
+    // fired when draw mode disabled
+  }
+};
+
+let eh = cy.edgehandles( eh_defaults );
+
 
