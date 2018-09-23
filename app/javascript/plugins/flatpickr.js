0e64771d43b91e1cbb3df71d998c102ac146f5b8
Thomas Pouillevet <thomas.pouillevet@gmail.com>
2018-09-23 14:00:19 +0200

    calendar range fixed & contact button alignment fixed

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -9,10 +9,10 @@ flatpickr(".datepicker", {
   altFormat: "F j, Y",
 })
 
-// flatpickr("#range_start", {
-//   mode: "range",
-//   minDate: "today",
-//   altInput: true,
-//   altFormat: "F j, Y",
-//   plugins: [new rangePlugin({ input: "#range_end"})]
-// })
+flatpickr("#range_start", {
+  mode: "range",
+  minDate: "today",
+  altInput: true,
+  altFormat: "F j, Y",
+  plugins: [new rangePlugin({ input: "#range_end"})]
+})
