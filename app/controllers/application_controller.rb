deafc7b6621caf1c5d6e6ad54305320bae479240
Alex Marichal <marichalalex@yahoo.fr>
2018-10-02 22:34:37 +0200

    added canadian trip to all new user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,37 +2,12 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
-  after_action :add_canadian_trip, if: :sign_up
 
   include Pundit
 
   # Pundit: white-list approach.
   after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
 
-  def add_canadian_trip
-    trip3 = Trip.new(
-    name: 'Road trip Canada - Adrien & Julie',
-    start_location: 'Montreal',
-    end_location: 'Montreal',
-    start_date: Date.new(2018,9,20),
-    end_date: Date.new(2018,9,24)
-    # reset_password_token: '',
-    # reset_password_sent_at: '',
-    # remember_created_at: '',
-    # created_at: ,
-    # updated_at", null: false,
-    # t.string "photo"
-  )
-trip3.user = current_user
-trip3.remote_photo_url = "https://images.unsplash.com/photo-1519885277449-12eee5564d68"
-trip3.save
-
-user_trip = UserTrip.new
-user_trip.user = user
-user_trip.trip = trip
-user_trip.save
-
-  end
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
---
+++ app/controllers/users/registrations_controller.rb
@@ -0,0 +1,77 @@
+# frozen_string_literal: true
+
+class Users::RegistrationsController < Devise::RegistrationsController
+  # before_action :configure_sign_up_params, only: [:create]
+  # before_action :configure_account_update_params, only: [:update]
+
+  # GET /resource/sign_up
+  # def new
+  #   super
+  # end
+
+  # POST /resource
+  def create
+    super
+    trip3 = Trip.new(
+    name: 'Road trip Canada - Adrien & Julie',
+    start_location: 'Montreal',
+    end_location: 'Montreal',
+    start_date: Date.new(2018,9,20),
+    end_date: Date.new(2018,9,24)
+    )
+    trip3.user = current_user
+    trip3.remote_photo_url = "https://images.unsplash.com/photo-1519885277449-12eee5564d68"
+    trip3.save!
+
+    # user_trip = UserTrip.new
+    # user_trip.user = current_user
+    # user_trip.trip = trip
+    # user_trip.save!
+  end
+
+  # GET /resource/edit
+  # def edit
+  #   super
+  # end
+
+  # PUT /resource
+  # def update
+  #   super
+  # end
+
+  # DELETE /resource
+  # def destroy
+  #   super
+  # end
+
+  # GET /resource/cancel
+  # Forces the session data which is usually expired after sign
+  # in to be expired now. This is useful if the user wants to
+  # cancel oauth signing in/up in the middle of the process,
+  # removing all OAuth session data.
+  # def cancel
+  #   super
+  # end
+
+  # protected
+
+  # If you have extra params to permit, append them to the sanitizer.
+  # def configure_sign_up_params
+  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
+  # end
+
+  # If you have extra params to permit, append them to the sanitizer.
+  # def configure_account_update_params
+  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
+  # end
+
+  # The path used after sign up.
+  # def after_sign_up_path_for(resource)
+  #   super(resource)
+  # end
+
+  # The path used after sign up for inactive accounts.
+  # def after_inactive_sign_up_path_for(resource)
+  #   super(resource)
+  # end
+end
